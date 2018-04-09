0904103f059853f6eab7ee94d37b70ea3bccc2dc
JarekSopinski <jarek.sopinski@gmail.com>
2018-04-09 15:25:12 +0200

    Getting user's location on window's load

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Local Weather App
+# Local Weather App
 
 A simple app showing current weather in user's location. User can toggle between Fahrenheit and Celsius. Data is fetched from an open API.
 
---
+++ script.js
@@ -0,0 +1,37 @@
+//api.openweathermap.org/data/2.5/weather?lat=54&lon=18&APPID=db980b1e7e7f4209e7d4c6b9a782221d
+
+let userLatitude;
+let userLongitude;
+
+function getUserLocation() {
+
+    const handleSuccess = (location) => {
+        userLatitude = location.coords.latitude;
+        userLongitude = location.coords.longitude;
+        console.log(userLatitude, userLongitude);
+    };
+
+    const handleError = (error) => {
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+                alert("This app requires your coordinates to run. Please allow geolocation in your browser.");
+                break;
+            case error.POSITION_UNAVAILABLE:
+                alert("Location information is unavailable");
+                break;
+            case error.TIMEOUT:
+                alert("The request to get user location timed out");
+                break;
+            default:
+                alert("An unknown error occurred.");
+                break;
+        }
+    };
+
+    navigator.geolocation ?
+        navigator.geolocation.getCurrentPosition(handleSuccess, handleError)
+        :
+        alert("Your browser doesn't support geolocation")
+}
+
+window.addEventListener("load", getUserLocation);
