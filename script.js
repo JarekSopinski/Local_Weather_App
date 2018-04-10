94b44fdd3e1fbf29021efbed7b6900d7f3bee860
JarekSopinski <jarek.sopinski@gmail.com>
2018-04-10 12:48:03 +0200

    Refactored geolocation to work on Promises

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
-//api.openweathermap.org/data/2.5/weather?lat=54&lon=18&APPID=db980b1e7e7f4209e7d4c6b9a782221d
+const userPosition = {
+    latitude: null,
+    longitude: null
+};
 
-let userLatitude;
-let userLongitude;
+const getUserPosition = () => {
 
-const getUserLocation = () => {
-
-    const handleSuccess = (location) => {
-        userLatitude = location.coords.latitude;
-        userLongitude = location.coords.longitude;
-        console.log(userLatitude, userLongitude);
-    };
-
-    const handleError = (error) => {
-        switch(error.code) {
-            case error.PERMISSION_DENIED:
-                alert("This app requires your coordinates to run. Please allow geolocation in your browser.");
-                break;
-            case error.POSITION_UNAVAILABLE:
-                alert("Location information is unavailable");
-                break;
-            case error.TIMEOUT:
-                alert("The request to get user location timed out");
-                break;
-            default:
-                alert("An unknown error occurred.");
-                break;
-        }
-    };
-
-    navigator.geolocation ?
-        navigator.geolocation.getCurrentPosition(handleSuccess, handleError)
-        :
-        alert("Your browser doesn't support geolocation")
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    })
 
 };
 
-const getAPIurl = (latitude, longitude) => {
-    console.log(`api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=db980b1e7e7f4209e7d4c6b9a782221d`);
-    return `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=db980b1e7e7f4209e7d4c6b9a782221d`
+const handleGeolocationSuccess = (location) => {
+    userPosition.latitude = location.coords.latitude;
+    userPosition.longitude = location.coords.longitude;
+    console.log(userPosition);
 };
 
+const handleGeolocationError = (error) => {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            alert("This app requires your coordinates to run. Please allow geolocation in your browser.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out");
+            break;
+        default:
+            alert("An unknown error occurred.");
+            break;
+    }
+};
 
-window.addEventListener("load", getUserLocation);
-window.addEventListener("load", () => getAPIurl(userLatitude, userLongitude));
+const getWeather = () => {
+
+    getUserPosition()
+        .then(position => handleGeolocationSuccess(position))
+        .catch(error => handleGeolocationError(error))
+
+};
+
+window.addEventListener("load", getWeather);
